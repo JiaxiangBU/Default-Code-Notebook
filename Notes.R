b84095b1eff020d2714f176c91662275998b0837
JiaxiangBU <jli270@binghamton.edu>
2017-01-26 13:47:51 -0600

    Update Notes.R

--- Notes.R
+++ Notes.R
@@ -1,57 +1,34 @@
 
-# find num. of elements of col --------------------------------------------
-
-
+# Find num. of elements of col
 length()
 
-# download bptest, use the button at the top.
-# find the y_hat, use fitted or predict
-
+# Download bptest, use the button at the top.
+# Find the y_hat, use fitted or predict
 pchisq(value, df=k, lower.tail = F)
 
 # overall F-test, use linearHypothesis(fit, c("x1 = 0","x2 = 0", vcov=vcovHC(fit, type="HC0"))
 linearHypothesis(fit, c("x1 = 0","x2 = 0", vcov=vcovHC(fit, type="HC0"))
 
-load("~/Desktop/R data sets for 5e/fertil1.RData")
-
-
-# use guide of official resource ------------------------------------------
-
-
-library(shiny)
-runExample("01_hello")
-
-
-# Solve “NA” --------------------------------------------------------------
-
-
+# Solve “NA”, use na.omit
 myvars=c("lsalary","LSAT","GPA","llibvol","lcost","rank")
 newdata=data[myvars]
 newdata1=na.omit(newdata)
-nrow(newdata1)
+nrow(newdata1) #Check the No. which is reduced, if there exist NA rows.
 
-
-# find num. ---------------------------------------------------------------
-
-
+# Find No.
 inlfhat <- fitted(fit)
-infhar_gt1 <- subset(inlfhat,inlfhat>1)
+infhar_gt1 <- subset(inlfhat,inlfhat>1) # Divid the dataset
 length(infhar_gt1)
 infhar_gt0 <- subset(inlfhat,inlfhat<0)
 length(infhar_gt0)
 
-# rename interactively
-fix(data) # results are saved on close
-
-# rename programmatically
+# Rename programmatically
 library(reshape)
 mydata <- rename(mydata, c(oldname="newname"))
 
 
-# RENAME VARS -------------------------------------------------------------
-
-
-# you can re-enter all the variable names in order
-# changing the ones you need to change.the limitation
-# is that you need to enter all of them!
+# Or
+# You can re-enter all the variable names in order
+# changing the ones you need to change.the limitation is that you need to enter all of them!
 names(data) <- c("Num","Gender","F1", "F2","M1","M2","GPA","DQ","PV","SV","SR","RQ")
+                 # But I think it is a little bit waste of time.
